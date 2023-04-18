2770f6d0c3c2d1f3091a75089f3c782260742585
sasi.eth <me@sasi.codes>
2023-04-18 18:19:29 +0530

    chore: add fomat number

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import formatNumber from '@/utils/formatNumber';
+
 const Stats = () => {
   return (
     <div className="md:grid-col-2 grid gap-4 lg:grid-cols-4">
       <div className="flex flex-col space-y-1.5 rounded-xl bg-gray-50 px-6 py-5">
         <span className="text-xs font-medium uppercase opacity-50">Transactions</span>
-        <span className="text-2xl font-medium">435323</span>
+        <span className="text-2xl font-medium">{formatNumber(435999)}</span>
       </div>
       <div className="flex flex-col space-y-1.5 rounded-xl bg-gray-50 px-6 py-5">
         <span className="text-xs font-medium uppercase opacity-50">Transactions per second</span>
