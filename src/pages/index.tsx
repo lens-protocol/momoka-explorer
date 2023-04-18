0aa2ec20deacab5ce5bb06a838b6d4d1544fd1ad
sasi.eth <me@sasi.codes>
2023-04-18 18:01:26 +0530

    fix: typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ import Table from '@/components/Table';
 
 export default function Home() {
   return (
-    <div className="container mx-auto mt-5 max-w-[100rem] px-2 sm:px-6 lg:px-14">
+    <div className="container mx-auto max-w-[100rem] px-2 sm:px-6 lg:px-14">
       <Head>
         <title>Lens DA Explorer</title>
       </Head>
-      <div className="flex justify-center py-10 md:py-20">
+      <div className="flex justify-center py-10 md:py-28">
         <SearchBar />
       </div>
       <div className="mb-10 space-y-4">
