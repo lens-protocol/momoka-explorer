15a0dda0a13165bb463f01d4da18c88c9e71e01b
bigint <69431456+bigint@users.noreply.github.com>
2023-04-19 11:36:42 +0530

    feat: dark mode support for stats, search and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,28 @@
+import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { useTheme } from 'next-themes';
 import type { FC } from 'react';
 
 import Network from './Network';
 
 const Navbar: FC = () => {
+  const { setTheme, resolvedTheme } = useTheme();
   return (
-    <nav className="fixed z-10 mx-auto w-full max-w-full bg-white px-2 sm:px-6 lg:px-14">
+    <nav className="fixed z-10 mx-auto w-full max-w-full bg-white px-2 dark:bg-gray-800 sm:px-6 lg:px-14">
       <div className="flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center justify-center">
           <img className="h-8 w-auto" src="/logo.svg" alt="Lens" draggable={false} />
         </Link>
-        <Network />
+        <div className="flex items-center space-x-2">
+          <button
+            onClick={() => {
+              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+            }}
+          >
+            {resolvedTheme === 'dark' ? <SunIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
+          </button>
+          <Network />
+        </div>
       </div>
     </nav>
   );
--- src/components/home/SearchBar.tsx
+++ src/components/home/SearchBar.tsx
@@ -11,14 +11,14 @@ const SearchBar = () => {
   }, []);
 
   return (
-    <div className="flex w-full rounded-xl border bg-white px-2 md:max-w-[50%]">
+    <div className="flex w-full rounded-xl border bg-white px-2 dark:border-gray-950 dark:bg-gray-800 md:max-w-[50%]">
       <span className="flex select-none items-center pl-3">
         <MagnifyingGlassIcon className="h-5 w-5 opacity-50" />
       </span>
       <input
         ref={inputElement}
         type="text"
-        className="flex-1 bg-transparent px-6 py-4 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none sm:leading-6 md:text-lg"
+        className="flex-1 bg-transparent px-6 py-4 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none dark:text-gray-100 sm:leading-6 md:text-lg"
         placeholder="Search transaction Id"
         autoComplete="off"
         autoFocus
