cd9d9de8296dd0ce973e4bf9f579808910e22285
bigint <69431456+bigint@users.noreply.github.com>
2023-04-19 11:28:53 +0530

    fix: container width for tx page

--- src/pages/tx/[arweaveId].tsx
+++ src/pages/tx/[arweaveId].tsx
@@ -4,13 +4,13 @@ import Transaction from '@/components/Transaction';
 
 export default function Home() {
   return (
-    <div className="container mx-auto max-w-[100rem] px-2 sm:px-6 lg:px-14">
+    <>
       <Head>
         <title>Lens DA Explorer</title>
       </Head>
       <div className="mb-10">
         <Transaction />
       </div>
-    </div>
+    </>
   );
 }
