92a5df8f16544a5be4d16a2c7e7b728d2e97f6a3
bigint <69431456+bigint@users.noreply.github.com>
2023-04-19 11:55:43 +0530

    feat: dark mode support for tx page and add meta component

--- src/components/Transaction/index.tsx
+++ src/components/Transaction/index.tsx
@@ -2,46 +2,31 @@ import type { FC } from 'react';
 
 import { PublicationSortCriteria, usePublicationsQuery } from '@/generated';
 
+const Meta: FC = () => (
+  <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
+    <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-gray-300">GM</dt>
+    <dd className="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-200 sm:col-span-2 sm:mt-0">GM</dd>
+  </div>
+);
+
 const Transaction: FC = () => {
   const { data, loading } = usePublicationsQuery({
     variables: { request: { sortCriteria: PublicationSortCriteria.Latest, limit: 10 } }
   });
 
   return (
-    <div className="relative space-y-5 rounded-xl bg-gray-50 px-2 py-4 md:p-5">
-      <div>
-        <div>
-          <div className="px-4 sm:px-0">
-            <h3 className="text-base font-semibold leading-7 text-gray-900">Transaction Details</h3>
-            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-              All Transaction related information will be displayed here.
-            </p>
-          </div>
-          <div className="mt-6 border-t border-gray-100">
-            <dl className="divide-y divide-gray-100">
-              <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-gray-900">GM</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GM</dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-gray-900">GM</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GM</dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-gray-900">GM</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GM</dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-gray-900">GM</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GM</dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-gray-900">GM</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GM</dd>
-              </div>
-            </dl>
-          </div>
-        </div>
+    <div className="relative mt-6 space-y-4 rounded-xl border border-gray-100 bg-gray-50 px-2 py-4 dark:border-gray-950 dark:bg-gray-800 md:p-5">
+      <div className="px-4 sm:px-0">
+        <h3 className="font-medium opacity-80">Transaction Details</h3>
+        <p className="text-sm opacity-60">All Transaction related information will be displayed here.</p>
+      </div>
+      <div className="mt-6 border-t border-gray-200 dark:border-gray-900">
+        <Meta />
+        <Meta />
+        <Meta />
+        <Meta />
+        <Meta />
+        <Meta />
       </div>
     </div>
   );
