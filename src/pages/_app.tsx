9489d11a4665255bea94bed9d0c0e53139a5c145
bigint <69431456+bigint@users.noreply.github.com>
2023-04-19 11:31:23 +0530

    feat: add dark mode deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 import client from '@/apollo';
 import Navbar from '@/components/Navbar';
@@ -9,10 +10,12 @@ import Navbar from '@/components/Navbar';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Navbar />
-      <div className="container mx-auto max-w-[100rem] px-2 pt-16 sm:px-6 lg:px-14">
-        <Component {...pageProps} />
-      </div>
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Navbar />
+        <div className="container mx-auto max-w-[100rem] px-2 pt-16 sm:px-6 lg:px-14">
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
