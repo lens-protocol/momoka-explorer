33b7e39671301ab9f351c8915ac72da18b0000eb
bigint <69431456+bigint@users.noreply.github.com>
2023-04-21 10:13:49 +0530

    chore: add constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const STAGING_LENSTER_URL = 'https://staging.lenster.app';
+export const TESTNET_LENSTER_URL = 'https://testnet.lenster.app';
+export const PRODUCTION_LENSTER_URL = 'https://lenster.app';
