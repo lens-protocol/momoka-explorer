0bd796b78447bb0bffb7b9b40f856dcc16dbd8f1
bigint <69431456+bigint@users.noreply.github.com>
2023-04-21 18:08:55 +0530

    feat: add user menu

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,45 @@
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+import { useDisconnect } from 'wagmi';
+
+const UserMenu = () => {
+  const { disconnect } = useDisconnect();
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button>gm</Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="px-1 py-1 ">
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  className={`${
+                    active ? 'bg-gray-100' : 'text-gray-900'
+                  } group flex w-full items-center rounded-xl px-2 py-2 text-sm`}
+                  onClick={() => {
+                    disconnect?.();
+                  }}
+                >
+                  Logout
+                </button>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
+
+export default UserMenu;
