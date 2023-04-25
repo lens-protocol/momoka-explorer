a5bd43cec32d864e51c9cd0db2e5903b1c54b87d
sasi.eth <me@sasi.codes>
2023-04-25 15:22:57 +0530

    chore: cleanup

--- src/components/home/SearchBar.tsx
+++ src/components/home/SearchBar.tsx
@@ -89,7 +89,7 @@ const SearchBar = () => {
           <Link
             href={`/tx/${txn?.transactionId}`}
             onClick={() => storeToRecents(txn)}
-            className="flex w-full items-center justify-between rounded-2xl px-4 py-2 hover:bg-gray-100 hover:dark:bg-[#16161B]"
+            className="flex w-full items-center justify-between rounded-xl px-4 py-2 hover:bg-[#F1F8F3] hover:dark:bg-[#272E29]"
           >
             {txn?.transactionId} <span className="text-xs opacity-50">{txn.publicationId}</span>
           </Link>
@@ -100,7 +100,7 @@ const SearchBar = () => {
             <Link
               key={recent.transactionId}
               href={`/tx/${recent.transactionId}`}
-              className="flex w-full items-center justify-between rounded-2xl px-4 py-2.5 hover:bg-gray-100 hover:dark:bg-[#16161B]"
+              className="flex w-full items-center justify-between rounded-xl px-4 py-2.5 hover:bg-[#F1F8F3] hover:dark:bg-[#272E29]"
             >
               <span className="flex items-center gap-2 text-sm opacity-80">
                 <ClockIcon className="h-4 w-4" />
