4cc08962ae52829ac749a7edcfec36ac02d8ea2e
sasi.eth <me@sasi.codes>
2023-08-03 18:24:13 +0530

    remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
+  reactStrictMode: false
 };
 
 module.exports = nextConfig;
