2bd08b6fc4ea017bbae7a249c0d3c7efaf50e856
bigint <69431456+bigint@users.noreply.github.com>
2023-11-02 18:28:51 +0530

    fix: rawUri null error

--- src/components/Transaction/MoreDetails.tsx
+++ src/components/Transaction/MoreDetails.tsx
@@ -45,7 +45,9 @@ const MoreDetails: FC<MoreDetailsProps> = ({ momokaTransaction }) => {
               <ChevronUpIcon className={`${!open ? 'rotate-180 transform' : ''} h-5 w-5`} />
             </Disclosure.Button>
             <Disclosure.Panel className="pb-2 text-sm">
-              <Meta title="On-chain Content URI" value={(data?.publication as any)?.metadata.rawUri} />
+              {(data?.publication as any)?.metadata.rawUri ? (
+                <Meta title="On-chain Content URI" value={(data?.publication as any)?.metadata.rawUri} />
+              ) : null}
               <Meta
                 title="Transaction Data"
                 value={
